3d40fc4b71ba6aa4d1e42356c1f31815cd436b3e
sulthananakkayam <sulthananakkayam@gmail.com>
2025-03-06 11:36:27 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,11 @@
+<?php 
+ function get_random_code($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
