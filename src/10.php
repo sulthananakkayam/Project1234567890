018b93c02f32c0ad16d0c3d771873a74add28d47
sulthananakkayam <sulthananakkayam@gmail.com>
2025-03-15 03:47:54 +0000

    Add 10.php

---
+++ src/10.php
@@ -0,0 +1,11 @@
+
+<?php
+// get the current time and date
+$now = time();
+
+// format the current time and date
+$date = date("Y-m-d H:i:s", $now);
+
+// display the formatted date
+echo "The current time is: " . $date;
+?>
