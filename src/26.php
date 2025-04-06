bb0c2027f8ca91fc4b9781aebb9bc3e1fe668ffe
sulthananakkayam <sulthananakkayam@gmail.com>
2025-04-06 01:51:54 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,7 @@
+<?php
+function displayRandomData() {
+    // Simulate fetching data from a database or API
+    $randomData = rand(1, 100); // Generate a random number between 1 and 100
+    echo "The generated random data is: " . $randomData;
+}
+?>
