9b953e7cfe372ae7e155e1101767603158c9e840
sulthananakkayam <sulthananakkayam@gmail.com>
2025-05-18 11:13:20 +0000

    Add 36.php

---
+++ src/36.php
@@ -0,0 +1,11 @@
+<?php
+// Project 1234567890 - A Cool School Project
+
+// Define your variables here
+$name = "Student Name";
+$age = 25;
+$subjects = array("Math", "English", "Science");
+
+// Display the project title and description here
+echo "Project Title: $name\nDescription: This is a cool school project.\n";
+?>
